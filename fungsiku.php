9bf319d56118320c7066e46559c8253613d03ac5
Ramaaditia <adhytt0@gmail.com>
2025-04-13 22:53:53 +0700

    Add files via upload
    Peraktikum1

--- fungsiku.php
+++ fungsiku.php
@@ -1,18 +1,12 @@
 <?php 
-/**
- * fungsi untuk menghitung nilai akhir mahasiswa
- * @param $nilai_uts Nilai UTS (0-100)
- * @param $nilai_uas Nilai UAS (0-100)
- * @param $nilai_tugas Nilai Tugas/Praktikum (0-100)
- * @return Nilai akhir mahasiswa 
- */
-function nilai_akhir($nilai_uts, $nilai_uas, $nilai_tugas) {
-    define("NILAI_UTS", 0.3);
-    define("NILAI_UAS", 0.3);
-    define("NILAI_TUGAS", 0.4);
-    $nilai_akhir = (NILAI_UTS * $nilai_uts) + (NILAI_UAS * $nilai_uas) + (NILAI_TUGAS * $nilai_tugas);
+function hitung_nilai($nilai_uts, $nilai_uas, $nilai_tugas) {
+    define ("NILAI_UTS", 0.25);
+    define ("NILAI_UAS", 0.30); 
+    define ("NILAI_TUGAS", 0.45);
+    $nilai_akhir = $nilai_uts * NILAI_UTS + $nilai_uas * NILAI_UAS+ $nilai_tugas * NILAI_TUGAS;
     return $nilai_akhir;
-}   
+}
+
 function kelulusan($nilai){
     define ("NILAI_MINIMAL", 60);
     if($nilai >= NILAI_MINIMAL){
